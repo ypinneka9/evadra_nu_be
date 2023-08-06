02ff57504f6f080dbf237746852db49ab518be55
ypinneka <yashwanth.260990@hotmail.com>
2023-08-05 21:41:51 -0700

    First Commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type person struct {
+    ID     int  `json:"id"`
+    Name  string  `json:"name"`
+    Email string  `json:"email"`
+    Phone  string `json:"phone"`
+	People  string `json:"people"`
+}
+
+var persons = []person{
+    {ID: 1, Name: "Yashwanth Pinneka", Email: "yashwanth.260990@gmail.com", Phone: "6312303767", People: "3"},
+    {ID: 2, Name: "Likhitha Pinneka", Email: "likhitha.komm@gmail.com", Phone: "6312303767", People: "3"},
+}
+
+func getPersons(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, persons)
+}
+
+func postPersons(c *gin.Context) {
+    var newPerson person
+
+    if err := c.BindJSON(&newPerson); err != nil {
+		fmt.Println("err = ", err)
+        return
+    }
+
+    persons = append(persons, newPerson)
+    c.IndentedJSON(http.StatusCreated, newPerson)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+func main() {
+    router := gin.Default()
+	router.Use(CORSMiddleware())
+	
+    router.GET("/persons", getPersons)
+	router.POST("/persons", postPersons)
+
+    router.Run("localhost:8080")
+}
