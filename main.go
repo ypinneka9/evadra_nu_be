ecef597b71427c1d68ec63d3a9a6193c18466d24
ypinneka <yashwanth.260990@hotmail.com>
2023-08-07 16:09:29 -0700

    Add Delete

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -37,12 +38,29 @@ func postPersons(c *gin.Context) {
     c.IndentedJSON(http.StatusCreated, newPerson)
 }
 
+func deletePerson(c *gin.Context) {
+    id := c.Param("id")
+    num, err := strconv.Atoi(id)
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+
+    for i, a := range persons {
+        if a.ID == num + 1 {
+            persons = append(persons[:i], persons[i+1:]...)
+            c.IndentedJSON(http.StatusOK, gin.H{"message": "person successfully deleted"})
+            return
+        }
+    }
+}
+
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
@@ -65,24 +83,8 @@ func main() {
 	
     router.GET("/persons", getPersons)
 	router.POST("/persons", postPersons)
+    router.DELETE("/persons/:id", deletePerson)
 
-    // router.Run("localhost:8080")
-    router.Run(":" + port)
+    router.Run("localhost:8080")
+    // router.Run(":" + port)
 }
-
-// port := os.Getenv("PORT")
-
-// if port == "" {
-//     log.Fatal("$PORT must be set")
-// }
-
-// router := gin.New()
-// router.Use(gin.Logger())
-// router.LoadHTMLGlob("templates/*.tmpl.html")
-// router.Static("/static", "static")
-
-// router.GET("/", func(c *gin.Context) {
-//     c.HTML(http.StatusOK, "index.tmpl.html", nil)
-// })
-
-// router.Run(":" + port)
